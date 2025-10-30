33abffa4841e4c1d867d482406cd1b2dd703dcfe
utsahice <utsahic.encircle@gmail.com>
2025-10-30 15:47:51 +0530

    feat : add storyblok editable draft mode

--- src/app/api/draft/route.ts
+++ src/app/api/draft/route.ts
@@ -1,13 +1,12 @@
 import { draftMode } from "next/headers";
-import { NextServer } from "next/dist/server/next";
 import { NextRequest } from "next/server";
-import { url } from "inspector";
 import { redirect } from "next/navigation";
 
-export const Get = async(req:NextRequest)=>{
-    const {searchParams} = new URL(req.url);
-    const slug = searchParams.get("slug");
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const slug = searchParams.get("slug");
 
-    (await draftMode()).enable();
-    redirect(`/${slug}?${searchParams.toString()}`);
+  (await draftMode()).enable();
+
+  redirect(`/${slug}?${searchParams.toString()}`);
 }
