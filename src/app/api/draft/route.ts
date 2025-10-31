989984d1b5768a6e1d4fb45d3763e34d91c378c0
utsahice <utsahic.encircle@gmail.com>
2025-10-31 11:48:19 +0530

    fix : live preview issue

--- src/app/api/draft/route.ts
+++ src/app/api/draft/route.ts
@@ -1,12 +1,13 @@
-import { draftMode } from 'next/headers'
-import { redirect } from 'next/navigation'
-import { NextRequest } from 'next/server'
+import { draftMode } from "next/headers";
+import { redirect } from "next/navigation";
+import { NextRequest } from "next/server";
+
 export const GET = async (request: NextRequest) => {
-  const { searchParams } = new URL(request.url)
-  const slug = searchParams.get('slug')
+  const { searchParams } = new URL(request.url);
+  const slug = searchParams.get("slug");
 
-  const draft = await draftMode()
-  draft.enable()
+  const draft = await draftMode();
+  draft.enable();
 
-  redirect(`/${slug}?${searchParams.toString()}`)
-}
+  redirect(`/${slug}?${searchParams.toString()}`);
+};
